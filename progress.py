89a670915e22bfe88a44f86d42b68d440f31906a
Antti Salonen <ajsalonen@gmail.com>
2012-11-04 07:54:45 +0100

    improve progress display

--- progress.py
+++ progress.py
@@ -33,7 +33,7 @@ class Progress:
         s = u''
         s += u'Progress: %d leagues in the queue, %d processed\n' % (len(self.leagues), len(self.processedleagues))
         if self.processedleagues:
-            s += u'%40s    %5s %5s %5s %5s %5s %5s %5s\n' % ('League', '1', '2', '3', '4', '5', '6', '7')
+            s += u'%50s    %7s %7s %7s %7s %7s %7s %7s %7s %7s %7s\n' % ('League', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
             for l, data in sorted(self.processedleagues.items()):
                 if data.levelnum == 1:
                     teaminfo = dict()
@@ -52,16 +52,16 @@ class Progress:
                             newDeps = set(rel.relegationleagues.keys())
                             newDeps -= visitedDeps
                             if newDeps:
-                                openDeps |= newDeps
+                                openDeps |= (newDeps - set([dep])) # avoid cycles
                             if rel.levelnum not in teaminfo:
                                 teaminfo[rel.levelnum] = [(rel.totalCompleteTeams, rel.getTotalNumTeams())]
                             else:
                                 teaminfo[rel.levelnum].append((rel.totalCompleteTeams, rel.getTotalNumTeams()))
 
-                    s += u'%40s => ' % data.title
-                    for i in xrange(1, 8):
+                    s += u'%50s => ' % data.title
+                    for i in xrange(1, 11):
                         if i not in teaminfo:
-                            s += u'%5s ' % '-'
+                            s += u'%7s ' % '-'
                         else:
                             if len(teaminfo[i]) == 1:
                                 stats = list(teaminfo[i][0])
@@ -69,9 +69,9 @@ class Progress:
                                     stats[0] = 0
                                 if not stats[1]:
                                     stats[1] = 0
-                                s += u'%2d/%2d ' % (stats[0], stats[1])
+                                s += u'%3d/%3d ' % (stats[0], stats[1])
                             else:
-                                s += u'%4dL ' % len(teaminfo[i])
+                                s += u'%6dL ' % len(teaminfo[i])
                     s += u'\n'
 
         return s
